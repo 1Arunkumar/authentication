b94466506693462212c3dd342c414537b98700c0
N R Arun Kumar <87988675+1Arunkumar@users.noreply.github.com>
2023-08-25 16:37:33 +0530

    Update user_controller.js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,19 +3,13 @@ const User = require('../models/user'); // Assuming the user model is defined in
 const bcrypt = require('bcrypt');
 
 // Controller method for rendering habit page after checking authentication
-module.exports.in = async function(req, res) {
-    if (req.isAuthenticated()) {
-        let user = await User.findOne({ email: req.body.email });
-        res.cookie('user_id', user.id); // Set user_id cookie
-        return res.render('habit', {
-            title: "habit"
-        });
-    }
-    return res.render('signin'); // Render signin page if not authenticated
-};
+
 
 // Controller method for rendering signup page
 module.exports.up = function(req, res) {
+    if(req.isAuthenticated){
+    return res.render('habit');
+    }
     return res.render('signup');
 };
 
@@ -25,6 +19,7 @@ module.exports.create = async function(req, res) {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             console.log('User already exists');
+            req.flash('error','user already exists');
             return res.redirect('/users/sign-in');
         }
         if (req.body.password === req.body.C_password) {
@@ -32,28 +27,38 @@ module.exports.create = async function(req, res) {
                 email: req.body.email,
                 password: req.body.password
             });
+            req.flash('success','sign up succesfull');
             console.log('User created');
             return res.redirect('/');
         }
     } catch (err) {
+        req.flash('error',err);
         console.log(err);
     }
 };
 
 // Controller method for creating a user session
-module.exports.createsession = function(req, res) {
+module.exports.createsession = async function(req, res) {
+    let user = await User.findOne({ email: req.body.email });
+    res.cookie('user_id', user.id);
+ 
+    req.flash('success','signin successfull')
     return res.render('habit', {
-        title: "habit"
+        title: "habit",
+        
     });
+    
 };
 
 // Controller method for destroying a user session (logout)
 module.exports.destroySession = function(req, res) {
     req.logout(function(err) {
         if (err) {
+            req.flash('error',err);
             console.error('Error during logout:', err);
             return res.status(500).send('Internal Server Error');
         }
+        req.flash('success','sign out successfull');
         res.cookie('user_id', null); // Clear user_id cookie
         return res.redirect('/');
     });
@@ -73,15 +78,19 @@ module.exports.update = async function(req, res) {
 
                 user.password = hashedPassword; // Update user's password
                 await user.save();
+                req.flash('success','password reset succesfull,signin');
                 return res.redirect('/');
             }
             else{
+                req.flash('error','new password doesnt match confirm password');
                 return res.send('passwords doesnt match');
             }
         } else {
+            req.flash('error','incorrect current password');
             return res.status(401).send('Unauthorized');
         }
     } catch (err) {
+        req.flash('error',err);
         console.log(err);
     }
 };
